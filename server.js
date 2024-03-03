c3d8030ab91cc045d0e06a2b2707cbfb19ade2cc
unknown <AbduAllahGabbar>
2024-03-03 03:32:49 +0200

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const WebSocket = require("ws");
-const session = require('express-session');
+const session = require("express-session");
 
 const server = require("http").createServer(app);
 const wss = new WebSocket.Server({ server, path: "/websocket" });
@@ -16,17 +16,19 @@ const logger = require("./helpers/logging");
 require("./helpers/middleware")(app); // this is for calling middleware
 require("./helpers/websocket")(wss); // this is for calling middleware
 require("./helpers/db_handler")(); // this is for calling Data base
-app.get('/test', (req, res, next) => {
-  res.send("App express on vrecel is done")
- })
- app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}))
+app.get("/test", (req, res, next) => {
+  res.send("App express on vrecel is done");
+});
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 app.use("/api/", indexRoutes);
-app.use("/attachments", express.static(path.join(__dirname, "./attachments")));
+app.use(express.static(path.join(__dirname, "attachments")));
 app.use(express.static("public"));
 
 const port = process.env.PORT || 40001;
