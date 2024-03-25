c479455cce721905dc862e7ffcc169a6b10feb70
unknown <AbduAllahGabbar>
2024-03-25 17:54:03 +0200

    make it better

--- modules/routers/unit.router.js
+++ modules/routers/unit.router.js
@@ -4,16 +4,14 @@ const unitController = require("../controllers/unit.controller");
 const roles = require("../../helpers/roles");
 
 unitRouter.get("/", unitController.getAllData);
-unitRouter.post("/", roles.isAuthenticatedAsAdmin, unitController.create);
+unitRouter.post("/", unitController.create);
 unitRouter.get("/:id", unitController.findById);
 unitRouter.put(
   "/:id",
-  roles.isAuthenticatedAsAdmin,
   unitController.updateUnit
 );
 unitRouter.delete(
   "/:id",
-  roles.isAuthenticatedAsAdmin,
   unitController.deleteUnit
 );
 
