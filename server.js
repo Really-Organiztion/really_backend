a4d9627e071bfbfbec56900c5b137ef051a5d8b4
unknown <AbduAllahGabbar>
2025-07-02 16:19:47 +0300

    #06-08-2024

--- server.js
+++ server.js
@@ -35,9 +35,10 @@ app.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Max-Age", "86400");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200).end();
-  }
+  return res.status(200).end();
+  // if (req.method === "OPTIONS") {
+  //   return res.sendStatus(200).end();
+  // }
 
   next();
 });
