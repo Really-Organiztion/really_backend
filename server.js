97306fc6e5bb5324fa6e2d137eaadbed1f820dd1
unknown <AbduAllahGabbar>
2025-07-02 15:11:58 +0300

    #06-08-2024

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   // }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
+  res.setHeader('Access-Control-Max-Age', '86400');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
