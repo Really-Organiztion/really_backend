5f676eb45c34a6d7f49814effc643952cc733a2b
unknown <AbduAllahGabbar>
2025-07-02 15:02:42 +0300

    #06-08-2024

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const allowedOrigins = [
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
+  console.log(origin);
+  
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
