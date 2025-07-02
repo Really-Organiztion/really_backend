da181f1c65419d0c1f2569112f3b8306da2ef690
unknown <AbduAllahGabbar>
2025-07-02 15:06:54 +0300

    #06-08-2024

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   console.log(origin,"zzzzzzzzz");
   
-  if (allowedOrigins.includes(origin)) {
+  // if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-  }
+  // }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
