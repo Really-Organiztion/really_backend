560b6a2ebeedc353c1470fd83d04862b6e2079ee
unknown <AbduAllahGabbar>
2025-07-02 16:12:08 +0300

    #06-08-2024

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Max-Age", "86400");
   if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
+    return res.sendStatus(200).end();
   }
 
   next();
